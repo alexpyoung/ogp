9d0ceae9426a7b705ea45e18f149ef2273592f4c
Alex Young <github@alexyoung.io>
2026-04-30 13:30:53 -0400

    feat(Document): defines GRDB record

--- App/Data/PDFModel.swift
+++ App/Data/PDFModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import GRDB
 import SwiftData
 
 @Model
@@ -23,5 +24,34 @@ final class PDFModel {
         self.updatedAt = Date()
         self.remotePath = remotePath
         self.fileName = fileName
+    }
+}
+
+struct Document: Codable, Identifiable, Hashable, FetchableRecord, PersistableRecord {
+    
+    let id: String
+    let remotePath: String
+    let fileName: String
+    let createdAt: Date
+    private(set) var updatedAt: Date
+    
+    enum Columns {
+        static let id = Column(CodingKeys.id)
+        static let remotePath = Column(CodingKeys.remotePath)
+        static let fileName = Column(CodingKeys.fileName)
+        static let createdAt = Column(CodingKeys.createdAt)
+        static let updatedAt = Column(CodingKeys.updatedAt)
+    }
+    
+    init(remotePath: String, fileName: String) {
+        self.id = UUID().uuidString
+        self.createdAt = Date()
+        self.updatedAt = Date()
+        self.remotePath = remotePath
+        self.fileName = fileName
+    }
+    
+    mutating func willUpdate(_: Database, columns _: Set<String>) throws {
+        self.updatedAt = Date()
     }
 }
