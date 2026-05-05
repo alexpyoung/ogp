503f7d5cdbd6a26d407745874127727d9df12020
Alex Young <github@alexyoung.io>
2026-05-05 03:38:22 -0400

    chore(URLSession): removes unused method

--- App/Extensions/OGP+URLSession.swift
+++ App/Extensions/OGP+URLSession.swift
@@ -15,9 +15,4 @@ extension URLSession {
         config.httpShouldSetCookies = true
         self.init(configuration: config)
     }
-    
-    func string(from url: URL) async throws -> String {
-        let (data, _) = try await self.data(from: url)
-        return String(decoding: data, as: UTF8.self)
-    }
 }
